4aa9b507391c6263a047148c6d8684670a778747
IsaelGabriel <isaelgsfaria@gmail.com>
2024-03-19 21:44:20 -0300

    Add base skill selection
    - 'ui_accept' uses selected skill;
    - 'ui_left' and 'ui_right' move through the hand;
    - Selected skill gets 20px higher than the others.

--- nodes/battle_monster.gd
+++ nodes/battle_monster.gd
@@ -9,9 +9,37 @@ var monster: Monster = Monster.new("Test Monster")
 
 @onready var skill_card_prefab = load("res://nodes/skill_card.tscn")
 
+@onready var selected_skill_index = 0:
+	set(value):
+		var hand_number = len(skill_manager.hand)
+		for child in skill_card_container.get_children():
+			child.selected = false
+		if value < 0: selected_skill_index = hand_number - 1
+		elif value >= hand_number: selected_skill_index = 0
+		else: selected_skill_index = value
+		skill_card_container.get_child(selected_skill_index).selected = true
+		
+
 func _ready():
 	skill_manager.load_skills(monster.skills)
 	for skill in skill_manager.hand:
 		var skill_card = skill_card_prefab.instantiate()
 		skill_card.skill_name = skill["name"]
 		skill_card_container.add_child(skill_card)
+	await get_tree().create_timer(0.01).timeout
+	skill_card_container.get_child(0).selected = true
+
+func _process(_delta):
+	if Input.is_action_just_pressed("ui_left"):
+		selected_skill_index -= 1
+	if Input.is_action_just_pressed("ui_right"):
+		selected_skill_index += 1
+	if Input.is_action_just_pressed("ui_accept"):
+		skill_manager.call_skill_from_hand(selected_skill_index)
+		skill_card_container.remove_child(skill_card_container.get_child(selected_skill_index))
+		if skill_card_container.get_child_count() == 0:
+			for skill in skill_manager.hand:
+				var skill_card = skill_card_prefab.instantiate()
+				skill_card.skill_name = skill["name"]
+				skill_card_container.add_child(skill_card)
+		selected_skill_index = 0
--- scripts/skill_card.gd
+++ scripts/skill_card.gd
@@ -8,3 +8,11 @@ var skill_name: String = "Skill" :
 	set(value):
 		name_label.text = value
 		skill_name = value
+
+var selected = false : 
+	set(value):
+		if value:
+			position.y = -20
+		else:
+			position.y = 0
+		selected = value
