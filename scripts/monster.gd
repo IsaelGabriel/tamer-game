4d3041216579deb59a5c5f6d16ba99e92b65c36b
IsaelGabriel <isaelgsfaria@gmail.com>
2024-03-19 21:01:30 -0300

    Add base _init funciton to monster

--- scripts/monster.gd
+++ scripts/monster.gd
@@ -1,6 +1,11 @@
 class_name Monster
 
+var name = "Monster"
 var max_hp = 10
 var hp = 10
 
 var skills = ["Hit", "Hit", "Hit", "Hit", "Heal", "Heal", "Heal"]
+
+
+func _init(name:String):
+	self.name = name
